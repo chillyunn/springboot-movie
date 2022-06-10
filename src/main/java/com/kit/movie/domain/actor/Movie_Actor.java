7b6373767e3920c7f933c5378cf8b5f026088d2c
dbgusWKd <gks1949@naver.com>
2022-06-10 15:21:10 +0900

    220610/15:21

--- src/main/java/com/kit/movie/domain/actor/Movie_Actor.java
+++ src/main/java/com/kit/movie/domain/actor/Movie_Actor.java
@@ -1,10 +1,19 @@
 package com.kit.movie.domain.actor;
 
+import com.kit.movie.domain.movie.Movie;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @NoArgsConstructor
 public class Movie_Actor {//영화이름(FK), 배우이름(FK)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "MOVIE_ID")
+    private Movie movie;
 
 }
