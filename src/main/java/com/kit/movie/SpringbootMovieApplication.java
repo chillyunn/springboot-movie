807058046ae4ee380068bb52961df6d2d1328e38
giyun <dkwhkp0@naver.com>
2022-06-10 22:50:47 +0900

    header수정

--- src/main/java/com/kit/movie/SpringbootMovieApplication.java
+++ src/main/java/com/kit/movie/SpringbootMovieApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootMovieApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMovieApplication.class, args);
 	}
